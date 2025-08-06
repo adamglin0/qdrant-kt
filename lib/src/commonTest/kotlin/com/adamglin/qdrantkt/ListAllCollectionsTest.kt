470c43abd49666fe80d34960cb75f1fbdb8c0cf4
adamglin <wwalkingg@gmail.com>
2025-08-06 14:51:54 +0800

    add unit tests for listAllCollections responses and edge cases

--- lib/src/commonTest/kotlin/com/adamglin/qdrantkt/ListAllCollectionsTest.kt
+++ lib/src/commonTest/kotlin/com/adamglin/qdrantkt/ListAllCollectionsTest.kt
@@ -2,6 +2,7 @@ package com.adamglin.qdrantkt
 
 import com.adamglin.qdrantkt.collections.Collections
 import com.adamglin.qdrantkt.collections.listAllCollections
+import com.adamglin.qdrantkt.domain.QdrantResponse
 import de.infix.testBalloon.framework.testSuite
 import io.ktor.client.engine.mock.*
 import io.ktor.http.*
@@ -40,12 +41,13 @@ val listAllCollectionsByMockTestSuite by testSuite {
 
         val client = QdrantClient(
             httpClientEngine = mockEngine,
-            url = "localhost"
+            url = "localhost",
         )
 
         val response = client.listAllCollections()
 
-        assertEquals("ok", response.status)
+        val successResponse = response as QdrantResponse.Success<Collections>
+        assertEquals("ok", successResponse.status)
         assertEquals(
             expected = Collections(
                 collections = listOf(
@@ -53,7 +55,64 @@ val listAllCollectionsByMockTestSuite by testSuite {
                     Collections.Collection("test_collection2")
                 )
             ),
-            actual = response.result,
+            actual = successResponse.result,
         )
+    }
+    test("listAllCollections should return an empty list of collections") {
+        val mockEngine = MockEngine { request ->
+            assertEquals("/collections", request.url.encodedPath)
+            respond(
+                content = """
+                    {
+                      "usage": {
+                        "cpu": 1,
+                        "payload_io_read": 1,
+                        "payload_io_write": 1,
+                        "payload_index_io_read": 1,
+                        "payload_index_io_write": 1,
+                        "vector_io_read": 1,
+                        "vector_io_write": 1
+                      },
+                      "time": 0.002,
+                      "status": "ok",
+                      "result": {
+                        "collections": []
+                      }
+                    }
+                """,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+        val client = QdrantClient(
+            httpClientEngine = mockEngine,
+            url = "localhost",
+        )
+        val response = client.listAllCollections()
+        val successResponse = response as QdrantResponse.Success<Collections>
+        assertEquals("ok", successResponse.status)
+        assertEquals(Collections(collections = emptyList()), successResponse.result)
+    }
+    test("listAllCollections should return an error response") {
+        val mockEngine = MockEngine { request ->
+            assertEquals("/collections", request.url.encodedPath)
+            respond(
+                content = """
+                {
+                    "status": {
+                        "error": "Not found"
+                    },
+                    "time": 0.0
+                }
+                """,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+        val client = QdrantClient(
+            httpClientEngine = mockEngine,
+            url = "localhost",
+        )
+        val response = client.listAllCollections()
+        val errorResponse = response as QdrantResponse.Error
+        assertEquals("Not found", errorResponse.status.error)
     }
 }
